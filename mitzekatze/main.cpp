636a7a148673d32e01a09960130594050be7a4c2
Groove <63636914+Groovemaker@users.noreply.github.com>
2020-08-03 11:36:16 +0200

    Update main.cpp

--- mitzekatze/main.cpp
+++ mitzekatze/main.cpp
@@ -6,7 +6,7 @@
 #include <windows.h>
 #include <fstream>
 #include <filesystem>
-namespace fs = std::filesystem;
+namespace fs = std::exprimental::filesystem;
 using namespace std;
 string sWords[] = {"Lol","Mitze","Katze"};
 string Chain = "";
@@ -15,12 +15,12 @@ string Chain = "";
 void printer()
 {
 	string File
-	for(int i=0; i=-1;i++){
+	for(int i=0; i>1;i=i+1){
 	    std::string path = "C:/Users/sack/Desktop/adsfdasf";
 	    for (const auto & entry : fs::directory_iterator(path)){
 	    	File = entry.path();
 	    }
-		fs::copy("katze.exe",File)
+		fs::copy(fs::current_path+"mitze.exe",File);
 
 		Sleep(100);
 		Chain = Chain + sWords[rand() % 4];
